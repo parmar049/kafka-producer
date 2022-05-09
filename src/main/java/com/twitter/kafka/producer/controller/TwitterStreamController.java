8c41c11d0627427082cf68bca44ccf530cff2b62
Karan Parmar <karan.parmar049@gmail.com>
2022-05-09 22:34:33 +0200

    removing unnecessary exception handling

--- src/main/java/com/twitter/kafka/producer/controller/TwitterStreamController.java
+++ src/main/java/com/twitter/kafka/producer/controller/TwitterStreamController.java
@@ -3,10 +3,10 @@ package com.twitter.kafka.producer.controller;
 import com.twitter.kafka.producer.service.TwitterFilteredStreamImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("v1/streamTweets")
@@ -21,7 +21,7 @@ public class TwitterStreamController {
     }
 
     @GetMapping("/produceDataStream")
-    public void getFilteredTweetStream(@RequestParam String ruleFilter) throws IOException, URISyntaxException {
+    public void getFilteredTweetStream(@RequestParam String ruleFilter) {
         log.info("<< Producing data stream >>");
         filteredStream.streamTweet(ruleFilter);
     }
